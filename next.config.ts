c17a269f977c86730b93b50ce2fc820eaff1785a
Shadik Mahamud <96811195+shadikmahamudsohan@users.noreply.github.com>
2024-12-10 19:39:48 +0000

    auth added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        serverActions: {
+          allowedOrigins: [
+            'localhost:3000', // localhost
+            'https://ubiquitous-space-acorn-jpjwvxp564gc575q-3000.app.github.dev', // Codespaces
+          ],
+        },
+      },
 };
 
 export default nextConfig;
