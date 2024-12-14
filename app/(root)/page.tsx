d6ec4ddaed7f39da522b3459dcb8b9387705b651
Shadik Mahamud <96811195+shadikmahamudsohan@users.noreply.github.com>
2024-12-14 17:44:01 +0000

    recommended section added and sanity added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,20 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "John Doe" },
+      _id: 1,
+      descripton: "This is a description",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/220px-Grosser_Panda.JPG",
+      category: "Technology",
+      title: "Panda Robots",
+    },
+  ];
 
   return (
     <>
@@ -18,6 +33,22 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search Results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="text-center">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    experimental: {
-        serverActions: {
-          allowedOrigins: [
-            'localhost:3000', // localhost
-            'https://ubiquitous-space-acorn-jpjwvxp564gc575q-3000.app.github.dev', // Codespaces
-          ],
-        },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:3000", // localhost
+        "https://ubiquitous-space-acorn-jpjwvxp564gc575q-3000.app.github.dev", // Codespaces
+      ],
+    },
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
       },
+    ],
+  },
 };
 
 export default nextConfig;
